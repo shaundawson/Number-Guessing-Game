efc1ecc8ff9c85f3b7f731dee943e2707fac2bcf
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-05 13:10:37 -0400

    Check the users guess. Repeat if guess is wrong.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def check_answer(guess, answer):
     elif guess < answer:
         print("Too low.")
     else: 
-        print(f"You got it! The answer was {answer}")
+        print(f"You got it! The answer was {answer}.")
 
 #Function to set difficulty
 def set_difficulty():
@@ -21,17 +21,26 @@ def set_difficulty():
     else: 
         return HARD_LEVEL_TURNS
 
-#Choose a random number between 1 and 100
-print("Welcome to the Number Guessing Game!")
-print("I\'m thinking of a number between 1 and 100.")
-answer = random.randint(1,100)
-print(f"Psst, the correct answer is {answer}")
-
-turns = set_difficulty()
-print(f"You have {turns} remaining to guess a number.")
-
-#Ask the user guess a number
-guess = int(input("Make a guess: "))
+def game():
+    #Choose a random number between 1 and 100
+    print("Welcome to the Number Guessing Game!")
+    print("I\'m thinking of a number between 1 and 100.")
+    answer = random.randint(1,100)
+    print(f"Psst, the correct answer is {answer}")
+    
+    turns = set_difficulty() #global variable
+    print(f"You have {turns} attempts remaining to guess a number.")
+    
+    
+    guess = 0 #global variable
+    while guess != answer:
+        #Ask the user guess a number
+        guess = int(input("Make a guess: "))
+        #Check the user's guess. Repeat the guessiing functionality if they get it wrong. 
+        check_answer(guess,answer)
 
 #Track the number of turns by 1 and reduce by 1 if they get it wrong
-#Repeating the guessing functionality  if they get it wrong
+
+
+
+game()
